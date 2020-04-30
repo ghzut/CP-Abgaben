7757e1811d5089e02ec7766ddb0112a9f580cc94
ghzut <33569260+ghzut@users.noreply.github.com>
2020-04-30 19:02:42 +0200

    TEX

--- Blatt1/A2/A2.py
+++ Blatt1/A2/A2.py
@@ -1,11 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from table2 import makeTable
 
 m,n = np.genfromtxt("A2/data/py_input_m_n.txt", unpack=True)
 x,y = np.genfromtxt("A2/data/py_input_x_y.txt", unpack=True)
-
-makeTable([x,y], r'{$x$} & {$y$}', 'data_tab', ['S[table-format=2.1]' , 'S[table-format=2.1]'], ["%2.1f", "%2.1f"])
 
 def f(x,m,n):
     return m*x+n
