c63320d73d61a015e698a010e78f7f0814cd43b4
ghzut <jonah.blank@tu-dortmund.de>
2020-05-03 14:42:27 +0200

    linear

--- Blatt2/A2/A2.cpp
+++ Blatt2/A2/A2.cpp
@@ -36,6 +36,31 @@ int main()
   }
   outfile.flush();
   outfile.close();
+  Profiler::resetAll();
+  ofstream outfile2("build/times_lin.txt", ofstream::trunc);
+  outfile2 << "#N,1,2,3\n";
+  for (int N = 1; N < 1001; ++N)
+  {
+  Profiler::start(0);
+  MatrixXd M = MatrixXd::Random(N,N);
+  VectorXd b = VectorXd::Random(N);
+  Profiler::stop(0);
+  MatrixXd L(N,N);
+  MatrixXd U(N,N);
+  MatrixXd P(N,N);
+  VectorXd x(N);
+  Profiler::start(1);
+  L = M.partialPivLu().matrixLU().triangularView<Eigen::UpLoType::UnitLower>();
+  U = M.partialPivLu().matrixLU().triangularView<Eigen::UpLoType::Upper>();
+  P = M.partialPivLu().permutationP();
+  Profiler::stop(1);
+  Profiler::start(2);
+  x = M.partialPivLu().solve(b);
+  Profiler::stop(2);
+  outfile2 << N << " " << Profiler::getTimeInS( 0 ) << " " << Profiler::getTimeInS( 1 ) << " " << Profiler::getTimeInS( 2 ) << "\n";
+  }
+  outfile2.flush();
+  outfile2.close();
 
   return 0;
 }
--- Blatt2/A2/A2.py
+++ Blatt2/A2/A2.py
@@ -10,4 +10,21 @@ plt.xscale("log")
 plt.yscale("log")
 plt.xlabel(r'$N$')
 plt.ylabel(r'$t/s$')
+plt.legend(loc='best')
 plt.savefig('A2/build/timers.pdf')
+
+N, t1, t2, t3 = np.genfromtxt("A2/build/times_lin.txt", unpack=True)
+
+plt.cla()
+plt.clf()
+plt.plot(N, t1, "b-", label = "Initialisierung")
+plt.plot(N, t2, "r-", label = "LU-Zerlegung")
+plt.plot(N, t3, "g-", label = "Lösung Mx=b")
+plt.xscale("log")
+plt.yscale("log")
+plt.xlabel(r'$N$')
+plt.ylabel(r'$t/s$')
+plt.legend(loc='best')
+plt.savefig('A2/build/timers_lim.pdf')
+
+
