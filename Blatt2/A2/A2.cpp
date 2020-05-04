03ec63ec1a32fa9824ccefe376790880d83b3237
ghzut <jonah.blank@tu-dortmund.de>
2020-05-04 13:32:44 +0200

    neue plots

--- Blatt2/A2/A2.cpp
+++ Blatt2/A2/A2.cpp
@@ -12,7 +12,7 @@ using namespace Eigen;
 
 int main()
 {
-  const auto nThreads = std::thread::hardware_concurrency();
+  const auto nThreads = thread::hardware_concurrency();
   Eigen::setNbThreads(nThreads);
   Profiler::init(3);
   ofstream outfile("build/times.txt", ofstream::trunc);
@@ -36,31 +36,34 @@ int main()
     x = M.partialPivLu().solve(b);
     Profiler::stop(2);
     outfile << N << " " << Profiler::getTimeInS( 0 ) << " " << Profiler::getTimeInS( 1 ) << " " << Profiler::getTimeInS( 2 ) << "\n";
+    Profiler::resetAll();
   }
   outfile.flush();
   outfile.close();
-  Profiler::resetAll();
+
   ofstream outfile2("build/times_lin.txt", ofstream::trunc);
   outfile2 << "#N,1,2,3\n";
-  for (int N = 1; N < 1001; ++N)
+
+  for (int N = 1; N < 2001; ++N)
   {
-  Profiler::start(0);
-  MatrixXd M = MatrixXd::Random(N,N);
-  VectorXd b = VectorXd::Random(N);
-  Profiler::stop(0);
-  MatrixXd L(N,N);
-  MatrixXd U(N,N);
-  MatrixXd P(N,N);
-  VectorXd x(N);
-  Profiler::start(1);
-  L = M.partialPivLu().matrixLU().triangularView<Eigen::UpLoType::UnitLower>();
-  U = M.partialPivLu().matrixLU().triangularView<Eigen::UpLoType::Upper>();
-  P = M.partialPivLu().permutationP();
-  Profiler::stop(1);
-  Profiler::start(2);
-  x = M.partialPivLu().solve(b);
-  Profiler::stop(2);
-  outfile2 << N << " " << Profiler::getTimeInS( 0 ) << " " << Profiler::getTimeInS( 1 ) << " " << Profiler::getTimeInS( 2 ) << "\n";
+    Profiler::start(0);
+    MatrixXd M = MatrixXd::Random(N,N);
+    VectorXd b = VectorXd::Random(N);
+    Profiler::stop(0);
+    MatrixXd L(N,N);
+    MatrixXd U(N,N);
+    MatrixXd P(N,N);
+    VectorXd x(N);
+    Profiler::start(1);
+    L = M.partialPivLu().matrixLU().triangularView<Eigen::UpLoType::UnitLower>();
+    U = M.partialPivLu().matrixLU().triangularView<Eigen::UpLoType::Upper>();
+    P = M.partialPivLu().permutationP();
+    Profiler::stop(1);
+    Profiler::start(2);
+    x = M.partialPivLu().solve(b);
+    Profiler::stop(2);
+    outfile2 << N << " " << Profiler::getTimeInS( 0 ) << " " << Profiler::getTimeInS( 1 ) << " " << Profiler::getTimeInS( 2 ) << "\n";
+    Profiler::resetAll();
   }
   outfile2.flush();
   outfile2.close();
