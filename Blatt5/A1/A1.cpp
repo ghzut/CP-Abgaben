89ebbe046c7867a69463edb536ecc297039abd1e
ghzut <33569260+ghzut@users.noreply.github.com>
2020-05-29 04:51:58 +0200

    so

--- Blatt5/A1/A1.cpp
+++ Blatt5/A1/A1.cpp
@@ -81,7 +81,7 @@ VectorXcd v_F_FFT(int n, const VectorXcd &v_f)
     {
       for(int a = 0; a < n/pow(2,i); ++a)
       {
-        v_Fj(a)= v_M_temp.at(i-1)(j, 2 * a) + v_M_temp.at(i-1)(j,2 * a + 1) * pow(-1., 2*j/pow(2,i));
+        v_Fj(a)= v_M_temp.at(i-1)(j, 2 * a) + v_M_temp.at(i-1)(j,2 * a + 1) * omega_j_N(j, pow(2,i));
       }
       M_i.row(j) = v_Fj;
       for(int b = 0; b < n/pow(2,i); ++b)
