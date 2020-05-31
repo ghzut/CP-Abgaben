662ab9b87f4257431c8fc4ef02be65deaa513d24
ghzut <33569260+ghzut@users.noreply.github.com>
2020-06-01 01:27:39 +0200

    was sind std::function?

--- Blatt6/A2/A2.cpp
+++ Blatt6/A2/A2.cpp
@@ -5,10 +5,22 @@
 using namespace std;
 using namespace Eigen;
 
+double f(VectorXd &x)
+{
+  return x.sum();
+}
 
+double bfgs(function<double(VectorXd)> f, const VectorXd &x0)//, MatrixXd C0, double epsilon)
+{
+  double result = f(x0);
+  return result;
+}
 
 int main()
-{   
-
+{
+  VectorXd x0(5);
+  x0 << 6, 8, 14, 4, 10;
+  double test = bfgs(f, x0);
+  cout << test << "= 42?" << endl;
   return 0;
 }
