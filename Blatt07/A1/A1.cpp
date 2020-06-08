2ae147aff754c7b162b2e6bb02434173f39ea81d
ghzut <jonah.blank@tu-dortmund.de>
2020-06-08 22:19:42 +0200

    sehr viele probleme

--- Blatt07/A1/A1.cpp
+++ Blatt07/A1/A1.cpp
@@ -5,190 +5,56 @@
 using namespace std;
 using namespace Eigen;
 
-//Definition der Rosenbrock-Funktion
-double rosenbrock(double x1, double x2)
+
+VectorXd get_r(double t, const VectorXd &v0)
 {
-    return (1-x1)*(1-x1) + 100*(x2-x1*x1)*(x2-x1*x1);
+  return v0;
 }
 
-//Funktion aus Aufgabe b)
-double f(double x1, double x2)
+VectorXd get_v(function<VectorXd(double, const VectorXd& )> f, double t, const VectorXd &r0)
 {
-    return 1/(1 + exp(-10*(x1*x2-3)*(x1*x2-3)/(x1*x1+x2*x2)));
+  return -r0;
 }
 
-double erste_ableitung(double (*f)(double, double), double x1, double x2, int k)
+VectorXd rk4(function<VectorXd(double, const VectorXd&)> func, const VectorXd &y0, double h)
 {
-    double h = 0.001;
-
-    if (k==0)
-    {
-        return (f(x1+h, x2) - f(x1-h, x2))/(2*h);
-    }
-    else
-    {
-        return (f(x1, x2+h) - f(x1, x2-h))/(2*h);
-    }   
-}
-
-double intervallhalbierung(double (*min)(double, double, double, double, double, double (*f)(double, double)), 
-                           double (*f)(double, double), double x1, double x2, double g1, double g2, 
-                           double a, double b, double c, double x_c)
-{
-    double d;
-    int n = 0;
-    
-    while (c-a > x_c)
-    {
-        if (b-a < c-b)
-        {
-            d = (c+b)/2;
-
-            if (min(d, x1, x2, g1, g2, f) < min(b, x1, x2, g1, g2, f))
-            {
-                a = b;
-                b = d;
-            }
-            else
-            {
-                c = d;
-            }
-        }
-        else
-        {
-            d = (a+b)/2;
-
-            if (min(d, x1, x2, g1, g2, f) < min(b, x1, x2, g1, g2, f))
-            {
-                c = b;
-                b = d;
-            }
-            else
-            {
-                a = d;
-            }
-            
-        }
-
-        n += 1;
-
-    }
-
-    return (c-a)/2;
-}
-
-double minimize(double lambda, double x1, double x2, double g1, double g2, double (*f)(double, double))
-{
-  return f(x1 + lambda*g1, x2 + lambda*g2);
-}
-
-void gradientenverfahren(double (*f)(double, double), Vector2d x_0)
-{
-    Vector2d g, x_analytisch;
-    double lambda, epsilon;
-    int n=0;
-
-    x_analytisch << 1,1;
-    epsilon = (x_0-x_analytisch).squaredNorm();
-
-    g(0) = erste_ableitung(f, x_0(0), x_0(1), 0)*(-1);
-    g(1) = erste_ableitung(f, x_0(0), x_0(1), 1)*(-1);
-
-    ofstream file;
-    file.open("build/gradientenverfahren.txt");
-    file << "# n      x1       x2       g1      g2      eps\n\n";
-    file << n << "  " << x_0(0) << "  " << x_0(1) << "  " << g(0) << "  " << g(1) << "  " << epsilon << "\n";
-
-
-    while (g.squaredNorm() > 0.005*0.005)
-    {
-
-        lambda = intervallhalbierung(minimize, rosenbrock, x_0(0), x_0(1), g(0), g(1), -50, 0, 50, 1e-6);
-        x_0 = x_0 + lambda * g;
-
-        n += 1;
-
-        g(0) = erste_ableitung(f, x_0(0), x_0(1), 0)*(-1);
-        g(1) = erste_ableitung(f, x_0(0), x_0(1), 1)*(-1);
-
-        if (n%10000==0)
-        {
-            epsilon = (x_0-x_analytisch).squaredNorm();
-            file << n << "  " << x_0(0) << "  " << x_0(1) << "  " << g(0) << "  " << g(1) << "  " << epsilon << "\n";
-        }
-    } 
-  
-    epsilon = (x_0-x_analytisch).squaredNorm();
-    file << n << "  " << x_0(0) << "  " << x_0(1) << "  " << g(0) << "  " << g(1) << "  " << epsilon << "\n";
-
-    file.close();
-  
-}
-
-void konjugiert(double (*f)(double, double), Vector2d x_0, string filename)
-{
-    Vector2d g, g_neu, p, x_analytisch;
-    double lambda, mu, epsilon;
-    int n=0;
-
-    x_analytisch << 1,1;
-    epsilon = (x_0-x_analytisch).squaredNorm();
-
-    g(0) = erste_ableitung(f, x_0(0), x_0(1), 0)*(-1);
-    g(1) = erste_ableitung(f, x_0(0), x_0(1), 1)*(-1);
-
-    p = g;
-
-    ofstream file;
-    file.open("build/konjugiert_"+filename+".txt");
-    file << "# n      x1       x2       g1      g2      eps\n\n";
-    file << n << "  " << x_0(0) << "  " << x_0(1) << "  " << g(0) << "  " << g(1) << "  " << epsilon << "\n";
-
-
-    while (g.squaredNorm() > 0.005*0.005)
-    {
-
-        lambda = intervallhalbierung(minimize, rosenbrock, x_0(0), x_0(1), p(0), p(1), -50, 0, 50, 1e-6);
-        x_0 = x_0 + lambda * p;
-
-        n += 1;
-
-        g_neu(0) = erste_ableitung(f, x_0(0), x_0(1), 0)*(-1);
-        g_neu(1) = erste_ableitung(f, x_0(0), x_0(1), 1)*(-1);
-
-        mu = (g_neu.dot(g_neu))/(g.dot(g));
-        g = g_neu;
-        p = g + mu * p;
-
-        epsilon = (x_0-x_analytisch).squaredNorm();
-        file << n << "  " << x_0(0) << "  " << x_0(1) << "  " << g(0) << "  " << g(1) << "  " << epsilon << "\n";
-        
-    } 
-
-    file.close();
+  VectorXd sum_k;
+  VectorXd k_1 = func(t0, r0);
+  VectorXd k_2 = func(t0 + h/2., r0 + k_1/2.);
+  VectorXd k_3 = func(t0 + h/2., r0 + k_2/2.);
+  VectorXd k_4 = func(t0 + h/2., r0 + k_3);
+  sum_k = k_1 + 2. * k_2 + 2. * k_3 + k_4;
+  sum_k *= h/6.;
+  return sum_k;
 }
 
 int main()
-{   
-
-    Vector2d x_0;
-    x_0 << -1,-1;
-
-    gradientenverfahren(&rosenbrock, x_0);
-    konjugiert(&rosenbrock, x_0, "rosenbrock");
-
-    x_0 << 1.5, 2.3;
-
-    konjugiert(&f, x_0, "b1");
-    
-    x_0 << -1.7, -1.9;
-
-    konjugiert(&f, x_0, "b2");
-
-    x_0 << 0.5, 0.6;
-
-    konjugiert(&f, x_0, "b3");
-
-
-    return 0;
+{
+  VectorXd rk;
+  VectorXd vk;
+  VectorXd r0;
+  VectorXd v0;
+  double err = 10.;
+  ofstream outfile("build/A1.txt", ofstream::trunc);
+  outfile << "#h, i, err\n"
+  for(double h = 1.; h > 1e-8; h/=10.)
+  {
+    r0 << 1.,1.;
+    v0 << 0.,0.;
+    vk = v0 + rk4(get_v, r0, h);
+    rk = r0 + rk4(get_r, vk, h);
+    err = (rk - r0).abs();
+    int k = 0;
+    outfile << h << " " << k+1 << " " << err << "\n";
+    for(k = 1; k < 10; ++k)
+    {
+      vk += rk4(get_v, rk, h);
+      rk += rk4(get_r, vk, h);
+      err = (rk-r0).abs();
+      outfile << h << " " << k+1 << " " << err << "\n";
+    }
+  }
+  outfile.flush();
+  outfile.close();
+  return 0;
 }
