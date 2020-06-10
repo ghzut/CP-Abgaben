932a34a614b93bd563b7496d999fce7471ab6e5d
ghzut <33569260+ghzut@users.noreply.github.com>
2020-06-10 15:22:49 +0200

    kleiner fehler

--- Blatt07/A1/A1.cpp
+++ Blatt07/A1/A1.cpp
@@ -16,13 +16,13 @@ VectorXd get_v(function<VectorXd(double, const VectorXd& )> f, double t, const V
   return -r*cos(t);
 }
 
-VectorXd rk4(function<VectorXd(double, const VectorXd&)> func, const VectorXd &y0, double h, double t0)
+VectorXd rk4(function<VectorXd(double, const VectorXd&)> func, const VectorXd &r0, double h, double t0)
 {
   VectorXd sum_k;
   VectorXd k_1 = func(t0, r0);
   VectorXd k_2 = func(t0 + h/2., r0 + k_1/2.);
   VectorXd k_3 = func(t0 + h/2., r0 + k_2/2.);
-  VectorXd k_4 = func(t0 + h/2., r0 + k_3);
+  VectorXd k_4 = func(t0 + h, r0 + k_3);
   sum_k = k_1 + 2. * k_2 + 2. * k_3 + k_4;
   sum_k *= h/6.;
   return sum_k;
@@ -30,14 +30,14 @@ VectorXd rk4(function<VectorXd(double, const VectorXd&)> func, const VectorXd &y
 
 int main()
 {
-  double t0 = 0;
+  double t0 = 0.;
   VectorXd rk;
   VectorXd vk;
   VectorXd r0;
   VectorXd v0;
   double err = 10.;
   ofstream outfile("build/A1.txt", ofstream::trunc);
-  outfile << "#h, i, err\n"
+  outfile << "#h, i, err\n";
   for(double h = 1.; h > 1e-8; h/=10.)
   {
     r0 << 1.,0.,0.;
