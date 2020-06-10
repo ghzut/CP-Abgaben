b72dad5232b303852ad5334cd6734c3e9ab14c59
ghzut <33569260+ghzut@users.noreply.github.com>
2020-06-10 16:53:45 +0200

    Energieerhaltung

--- Blatt07/A1/A1.cpp
+++ Blatt07/A1/A1.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 using namespace Eigen;
 
+double get_energy(const Vector3d &r)
+{
+  return pow(r.norm(),2)/2.;
+}
 
 Vector3d get_r(double t, const Vector3d &v)
 {
@@ -42,16 +46,15 @@ int main()
   ofstream outfile2("build/A1_Mat.txt", ofstream::trunc);
   outfile2 << "#Matrix der Ergebnisvektoren\n";
   outfile << "#h, i, err\n";
-  for(double h = 1.; h >= 1e-7; h/=10.)
+  r0 << 42.,42.,42.;
+  v0 << 0.,0.,0.;
+  for(double h = 1.; h >= 1e-5; h/=10.)
   {
     MatrixXd M = MatrixXd::Zero(3,10);
-    r0 << 42.,42.,42.;
-    v0 << 0.,0.,0.;
-    k = 0;
     vk = v0 + rk4(get_v, r0, h, double(k));
     rk = r0 + rk4(get_r, vk, h, double(k));
     err = (rk - r0).norm();
-    outfile << h << " " << k+1 << " " << err << "\n";
+    outfile << h << " " << 1 << " " << err << "\n";
     M.col(k) = rk;
     for(k = 1; k < 10; ++k)
     {
@@ -67,5 +70,26 @@ int main()
   outfile.close();
   outfile2.flush();
   outfile2.close();
+
+  //Aufgabenteil c) Energieerhaltung, es wird das maximale h verwendet das die Toleranz aus b erfüllt
+  //Für die Energie wird eigentlich eine Masse benötigt wegen E_i = m/2 * omega^2 *C_i^2. Diese wird hier m=1 gesetzt.
+  double h = 1e-3;
+  double energy = get_energy(r0);
+  ofstream outfile3("build/A1_c.txt", ofstream::trunc);
+  outfile3 << "#k, E\n";
+  outfile3 << 0 << " " << energy << "\n";
+  vk = v0 + rk4(get_v, r0, h, double(k));
+  rk = r0 + rk4(get_r, vk, h, double(k));
+  enegy = get_energy(rk)
+  outfile3 << 1 << " " << energy << "\n";
+  for(k = 1; k < 20; ++k)
+  {
+    vk += rk4(get_v, rk, h, double(k));
+    rk += rk4(get_r, vk, h, double(k));
+    enegy = get_energy(rk)
+    outfile3 << k+1 << " " << energy << "\n";
+  }
+  outfile3.flush();
+  outfile3.close();
   return 0;
 }
