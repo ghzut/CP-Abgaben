69b0a1747c28e00ef50090552aa46ba264a0c731
ghzut <33569260+ghzut@users.noreply.github.com>
2020-06-12 00:13:57 +0200

    cout

--- Blatt07/A1/A1.cpp
+++ Blatt07/A1/A1.cpp
@@ -17,6 +17,7 @@ double get_energy(const Vector3d &r, const Vector3d &v)
 Vector3d get_r_1(double t, const Vector3d &v)
 {
   Vector3d new_r = -v*sin(t);
+  if(int(t/M_PI)%2==1) cout << "r: " << new_r << endl << endl;
   return new_r;
 }
 
@@ -24,7 +25,7 @@ Vector3d get_v_1(double t, const Vector3d &r)
 {
   Vector3d new_v;
   new_v(0) = -r(0)*cos(t);
-  if(int(t/M_PI)%2==1) cout << new_v << endl << endl;
+  if(int(t/M_PI)%2==1) cout << "v" << new_v << endl << endl;
   return new_v;
 }
 
@@ -33,7 +34,6 @@ Vector3d get_r_2(double t, const Vector3d &v)
 {
   Vector3d new_r = -v*sin(t);
   new_r(1) = cos(t);
-  if(int(t/M_PI)%2==1) cout << sin(t) << endl << endl;
   return new_r;
 }
 
