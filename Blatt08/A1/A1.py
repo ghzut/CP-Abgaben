0989ba2450c018c928259dee51705c7a7be1bafc
Julia Sobolewski <julia.sobolewski@tu-dortmund.de>
2020-06-26 00:34:19 +0200

    merge

--- Blatt08/A1/A1.py
+++ Blatt08/A1/A1.py
@@ -4,18 +4,29 @@ import matplotlib.pyplot as plt
 t, vx, vy, ekin, epot, T = np.genfromtxt("A1/build/aequi1.txt", unpack=True)
 
 plt.plot(t, T)
+plt.xlabel("t")
+plt.ylabel("T")
 plt.savefig("A1/build/aequi1_T.pdf")
 
 plt.clf()
 
 plt.plot(t[:50], T[:50])
+plt.xlabel("t")
+plt.ylabel("T")
+
 plt.savefig("A1/build/aequi1_TT.pdf")
 
 plt.clf()
 
-plt.plot(t, ekin)
-plt.plot(t, epot)
-plt.plot(t, ekin+epot)
+plt.plot(t, ekin, label=r"$E_\mathrm{kin}$")
+plt.plot(t, epot, label=r"$E_\mathrm{pot}$")
+plt.plot(t, ekin+epot, label=r"$E$")
+
+plt.xlabel("t")
+plt.ylabel("E")
+
+plt.legend()
+
 
 plt.savefig("A1/build/aequi1_E.pdf")
 
@@ -23,23 +34,32 @@ plt.clf()
 
 E = ekin+epot
 
-plt.plot(t[:50], ekin[:50])
-plt.plot(t[:50], epot[:50])
-plt.plot(t[:50], E[:50])
+plt.plot(t[:50], ekin[:50], label=r"$E_\mathrm{kin}$")
+plt.plot(t[:50], epot[:50], label=r"$E_\mathrm{pot}$")
+plt.plot(t[:50], E[:50], label=r"$E$")
+
+plt.legend()
 
 plt.savefig("A1/build/aequi1_EE.pdf")
 
 plt.clf()
 
-plt.plot(t, vx)
-plt.plot(t, vy)
+plt.plot(t, vx, label=r"$v_x$")
+plt.plot(t, vy, label=r"$v_y$")
+
+plt.xlabel("t")
+plt.ylabel("v")
+
+plt.legend()
 
 plt.savefig("A1/build/aequi1_V.pdf")
 
 plt.clf()
 
-plt.plot(t[:50], vx[:50])
-plt.plot(t[:50], vy[:50])
+plt.plot(t[:50], vx[:50], label=r"$v_x$")
+plt.plot(t[:50], vy[:50], label=r"$v_y$")
+
+plt.legend()
 
 plt.savefig("A1/build/aequi1_VV.pdf")
 
@@ -49,6 +69,8 @@ plt.clf()
 t, T = np.genfromtxt("A1/build/messung1.txt", unpack=True)
 
 plt.plot(t, T)
+plt.xlabel("t")
+plt.ylabel("T")
 
 plt.savefig("A1/build/messung1_T.pdf")
 
@@ -64,6 +86,8 @@ plt.clf()
 t, T = np.genfromtxt("A1/build/messung001.txt", unpack=True)
 
 plt.plot(t, T)
+plt.xlabel("t")
+plt.ylabel("T")
 
 plt.savefig("A1/build/messung001_T.pdf")
 
@@ -79,6 +103,8 @@ plt.clf()
 t, T = np.genfromtxt("A1/build/messung100.txt", unpack=True)
 
 plt.plot(t, T)
+plt.xlabel("t")
+plt.ylabel("T")
 
 plt.savefig("A1/build/messung100_T.pdf")
 
@@ -91,9 +117,12 @@ print(T_mean, " +- ", T_std)
 
 t, vx, vy, ekin, epot, T = np.genfromtxt("A1/build/aequi_isokinetisch001.txt", unpack=True)
 
-plt.plot(t, ekin)
-plt.plot(t, epot)
-plt.plot(t, ekin+epot)
+plt.plot(t, ekin, label=r"$E_\mathrm{kin}$")
+plt.plot(t, epot, label=r"$E_\mathrm{pot}$")
+plt.plot(t, ekin+epot, label=r"$E$")
+
+plt.xlabel("t")
+plt.ylabel("E")
 
 plt.savefig("A1/build/aequi_isokinetisch001_E.pdf")
 
@@ -101,9 +130,12 @@ plt.clf()
 
 E = ekin+epot
 
-plt.plot(t[:50], ekin[:50])
-plt.plot(t[:50], epot[:50])
-plt.plot(t[:50], E[:50])
+plt.plot(t[:200], ekin[:200], label=r"$E_\mathrm{kin}$")
+plt.plot(t[:200], epot[:200], label=r"$E_\mathrm{pot}$")
+plt.plot(t[:200], E[:200], label=r"$E$")
+
+plt.xlabel("t")
+plt.ylabel("E")
 
 plt.savefig("A1/build/aequi_isokinetisch001_EE.pdf")
 
